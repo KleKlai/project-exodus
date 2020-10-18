1437a0f1d6cf9867d6331d1d458688e39865cd62
csolatorio <solatoriocarylethel@gmail.com>
2020-10-18 18:42:50 +0800

    updatesss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,65 +8,15 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-
+       <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <!-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -97,7 +47,107 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+        </div> -->
+        {{-- Navigation --}}
+            <div class="nav-container">
+                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(255, 255, 255, 0.25);">
+                    <a class="navbar-brand" href="#">
+                        <img src="{{ asset('images/logo/logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                    </a>
 
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/artworks">Artworks <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/artists">Artists</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/galleries">Galleries</a>
+                            </li>
+                        </ul>
+
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/login">
+                                    Login
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/register">
+                                    Sign Up
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    </nav>
+                        <div id="captioned-gallery">
+                            <figure class="slider row">
+                                <figure class="col-md">
+                                    <img src="/images/img/blog-1.jpg" class="img-fluid" alt>
+                                </figure>
+                                <figure class="col-md">
+                                    <img src="/images/img/blog-2.jpg" class="img-fluid" alt>
+                                </figure>
+                                <figure class="col-md">
+                                    <img src="/images/img/blog-3.jpg" class="img-fluid" alt>
+                                </figure>
+                                <figure class="col-md">
+                                    <img src="/images/img/blog-1.jpg" class="img-fluid" alt>
+                                </figure>
+                                <figure class="col-md">
+                                    <img src="/images/img/blog-3.jpg" class="img-fluid" alt>
+                                </figure>
+                            </figure>
+                        </div>
+                        
+                        <div class="container">
+                        <div class="row featured">
+                            <div class="col-md-4">
+                                <div>
+                                    <img src="/images/img/blog-3.jpg" class="img-circle img-thumbnail">
+                                        <p></p>
+                                        <a href="#" class="btn btn-link">View More</a>
+                                </div>
+                            </div>
+
+                        <div class="col-md-4">
+                            <div>
+                                <img src="/images/img/blog-2.jpg" class="img-circle img-thumbnail">
+                                <p></p>
+                                <a href="#" class="btn btn-link">View More</a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div>
+                                <img src="/images/img/blog-1.jpg" class="img-circle img-thumbnail">
+                                <p></p>
+                                <a href="#" class="btn btn-link">View More</a>
+                            </div>
+		                </div>
+	                </div>
+                </div>
+               
+                <div class="newsletter">
+                    <div class="container row">
+                        <div class="col-md-10">
+                            <span class="text">Get the latest art stories and collections by simply 'Subscribe'</span>
+
+                        </div>
+                        <div class="col-md-2">
+                            <button type="button" class="btn btn-outline-dark text-white border-white">Subcribe</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div></div>
     </body>
 </html>
